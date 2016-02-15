ffb14425376d02243c536f6a2d12ebab26fbd42b
Christophe Porteneuve <tdd@tddsworld.com>
2016-02-15 14:42:40 +0100

    Début étape 5 : bases pour Action Creators, Reducer et tests "Goals"

--- src/action-creators.js
+++ src/action-creators.js
@@ -1,3 +1,21 @@
+// Goal settings management
+
+export const ADD_GOAL = '@@GOALTRACKER/GOALS_ADD'
+export const REMOVE_GOAL = '@@GOALTRACKER/GOALS_DEL'
+export const UPDATE_GOAL = '@@GOALTRACKER/GOALS_UPDATE'
+
+export function addGoal (name, target, units) {
+  // Votre code ici
+}
+
+export function removeGoal (id) {
+  // Votre code ici
+}
+
+export function updateGoal (id, name, target, units) {
+  // Votre code ici
+}
+
 // Using the app everyday
 
 export const LOGIN = '@@GOALTRACKER/AUTH_LOGIN'
---
+++ src/reducers/goals.js
@@ -0,0 +1,19 @@
+import { ADD_GOAL, REMOVE_GOAL, UPDATE_GOAL } from '../action-creators'
+
+export default function goals (state = [], action) {
+  switch (action.type) {
+    case ADD_GOAL: {
+      // Votre code ici
+    }
+
+    case REMOVE_GOAL:
+      // Votre code ici
+
+    case UPDATE_GOAL: {
+      // Votre code ici
+    }
+
+    default:
+      return state
+  }
+}
