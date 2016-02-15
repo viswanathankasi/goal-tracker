5ccb733ede3c4b652eae67af6158244b62fdcb58
Christophe Porteneuve <tdd@tddsworld.com>
2016-02-15 14:36:26 +0100

    Début étape 4 : bases pour Action Creators, Reducer et tests "Current User"

---
+++ src/action-creators.js
@@ -0,0 +1,12 @@
+// Using the app everyday
+
+export const LOGIN = '@@GOALTRACKER/AUTH_LOGIN'
+export const LOGOUT = '@@GOALTRACKER/AUTH_LOGOUT'
+
+export function logIn (email, password) {
+  // Votre code ici
+}
+
+export function logOut () {
+  // Votre code ici
+}
