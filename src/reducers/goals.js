1096a6e550ec739a4097dc7652cb83b1d8f3a743
Christophe Porteneuve <tdd@tddsworld.com>
2016-03-10 16:37:30 +0100

    Code annoté

--- src/reducers/goals.js
+++ src/reducers/goals.js
@@ -1,31 +1,61 @@
+// Objectifs (reducer)
+// ===================
+
+// On se préoccupe de 3 actions…
 import { ADD_GOAL, REMOVE_GOAL, UPDATE_GOAL } from '../action-creators'
 
+// Par défaut, `goals` vaut `[]` (pas d’objectifs définis)
 export default function goals (state = [], action) {
   switch (action.type) {
+    // Ajout d’objectif
+    // ----------------
     case ADD_GOAL: {
+      // On prend les champs qui nous intéressent
       const { name, target, units } = action
+      // On calcule un nouvel ID d’objectif, qui vaut le maximum
+      // de ceux existants, plus un.
+      //
       // L’approche un poil moins « pure fonctionnelle » serait :
       // ```js
       // const id = Math.max(...state.map((goal) => goal.id), -1) + 1
       // ````
+      //
+      // Si aucun objectif n’est défini,
+      // on ferait un `Math.max()` sans argument, ce qui renverrait
+      // `-Infinity`, pas top ; par conséquent, on ajoute un dernier
+      // argument, `-1`, qui garantit une valeur valide au démarrage.
+      // Ainsi, le premier `id` sera fatalement zéro (-1 + 1).
       const id = state.reduce((max, { id }) => max > id ? max : id, -1) + 1
+      // Version “immutable” (génère une nouvelle valeur plutôt que
+      // de modifier le tableau existant) de `.push(…)`.
       return [...state, { id, name, target, units }]
     }
 
+    // Retrait d’objectif
+    // ------------------
     case REMOVE_GOAL:
       return state.filter(({ id }) => id !== action.id)
 
+    // Mise à jour (voire ajout) d’objectif
+    // ------------------------------------
     case UPDATE_GOAL: {
       const { id, name, target, units } = action
       const newGoal = { id, name, target, units }
+      // Si l’objectif existait bien dans le tableau, on crée le nouveau
+      // tableau en partant de l’existant pour ne remplacer que l’entrée idoine.
       if (state.find(({ id }) => id === action.id)) {
         return state.map((goal) => goal.id === id ? newGoal : goal)
       }
 
+      // Si ça se trouve, on ne l’avait pas en fait, cet objectif…  Ça ne
+      // devrait pas arriver, mais autant se blinder.  On l’ajoute à la fin,
+      // dans un tel as, comme un ajout mais en préservant l'ID.
       return [...state, newGoal]
     }
 
     default:
+      // Rappel : un *reducer* doit **toujours** renvoyer l’état
+      // sans modification si l’action n’est pas applicable.
       return state
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,18 @@
+// Configuration Webpack
+// =====================
+
+// La configuration de Webpack peut être assez velue…
+// Pour simplifier notre propre code de configuration,
+// une bonne solution consiste à passer par
+// [hjs-webpack](https://github.com/HenrikJoreteg/hjs-webpack),
+// une surcouche de Henrik Joreteg.  On peut se contenter
+// d’indiquer de quel fichier source partir, où publier
+// les fichiers de production en cas de build, éventuellement
+// si on souhaite du HTTPS, et on est prêts à partir.
+// La simple présence des principaux plugins et loaders
+// Webpack dans le fichier `package.json` et le dossier
+// `node_modules` entraînera leur configuration automatique.
+
 var DashboardPlugin = require('webpack-dashboard/plugin')
 var getConfig = require('hjs-webpack')
 var OfflinePlugin = require('offline-plugin')
@@ -8,7 +23,12 @@ var config = getConfig({
   clearBeforeBuild: '!(favicon.ico)'
 })
 
+// C’est un plugin non pris en charge automatiquement par
+// hjs-webpack, on l’ajoute donc manuellement à la liste
+// définie par celui-ci.
 config.plugins.push(new OfflinePlugin())
+// Idem, plugin custom, qui nous fournit le dashboard Webpack
+// en ligne de commande.
 config.plugins.push(new DashboardPlugin({ port: 3002 }))
 
 module.exports = config
