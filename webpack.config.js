01ba46d3150b55cd5576bd50bd6708721ef3f88f
Christophe Porteneuve <tdd@tddsworld.com>
2016-03-30 11:25:01 +0200

    Manifeste applicatif W3C avec icônes, intégré au build

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,16 @@
+var CopyWebpackPlugin = require('copy-webpack-plugin')
 var DashboardPlugin = require('webpack-dashboard/plugin')
 var getConfig = require('hjs-webpack')
 var OfflinePlugin = require('offline-plugin')
 
 var config = getConfig({
+  html: process.env.NODE_ENV !== 'production',
   in: 'src/index.js',
   out: 'public',
   clearBeforeBuild: '!(favicon.ico)'
 })
 
+config.plugins.push(new CopyWebpackPlugin([{ from: 'static' }]))
 config.plugins.push(new OfflinePlugin())
 config.plugins.push(new DashboardPlugin({ port: 3002 }))
 
